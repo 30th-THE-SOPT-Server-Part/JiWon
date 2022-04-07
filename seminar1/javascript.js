33185356fe7cc5c701544c7bf6c84a2c592fbaca
jiwon_ryu <happyjw3910@gmail.com>
2022-04-07 19:12:56 +0900

    [ADD] Level1

--- seminar1/javascript.js
+++ seminar1/javascript.js
@@ -1,12 +1,15 @@
-/*
+/**
+ * @변수 var, let, const
+ */
 var name = '유지원';
 var name = '채정아';
 
 let name2 = '유지원';
 //let name2 = '채정아'; //재선언 불가
-*/
 
-/*
+/**
+ * @Scope function scope(var), block scope(const)
+ */
 if(true){
     var x = 'abc';
 }
@@ -15,10 +18,8 @@ console.log(x);
 if(true){
     const y = 'abc'; 
 }
-console.log(y); //const는 block 스코프여서 밖에서 사용할 수 없다.
-*/
+//console.log(y); //const는 block 스코프여서 밖에서 사용할 수 없다.
 
-/*
 function foo(){
     if(true){
         var n = '유지원';
@@ -26,16 +27,54 @@ function foo(){
     }
     console.log('function-block-',n);
 }
-console.log('global-',n); //var 변수는 function 스코프여서 글로벌로 사용 불가능하다.
-*/
+//console.log('global-',n); //var 변수는 function 스코프여서 글로벌로 사용 불가능하다.
 
-//========
+/**
+ * @Basic
+ */
 const name = '유지원';
-console.log(typeof name);
-const age = 20;
-console.log(`안녕하세요 제 이름은 ${name}입니다. 제 나이는 ${age} 살 입니다.`);
+console.log(typeof name); //typeof
 
-//========
+const age = 20;
+console.log(`안녕하세요 제 이름은 ${name}입니다. 제 나이는 ${age} 살 입니다.`); //`` 사용(string concat)
+
+/**
+ * @Array,Object
+ */
+ let arr = [1, 'item', true]; //여러 타입 들어갈 수 있음
+ let arr2 = Array(4,null,{item:'item'});
+
+ //Array.map()
+let num = [1,2,3,4];
+const newNumArr = arr.map(x => {
+	x * 2
+});
+//>> [2,4,6,8]
+
+/**
+ * @Function
+ */
+//함수 선언식
+ function sum(a,b){
+	return a+b;
+}
+//함수 표현식
+let sum = (a,b) => {
+	return a+b;
+}
+sum(); //으로 호출
+//함수 파라미터 사용
+const func = (num) => {
+	return num * num;
+}
+
+const multiple = (func, num) => {
+	console.log(func(num));
+}
+
+multiple(func, 3);
+//>> 9
+
 const sopt = {
     season: 30,
     group: ['YB','OB'],
